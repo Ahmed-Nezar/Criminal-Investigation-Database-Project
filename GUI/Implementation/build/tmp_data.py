e3fff8521f513b3ed072890754a2d024bc561b64
Ahmed-Nezar <ahmednezarrr@gmail.com>
2024-04-19 11:41:40 +0200

    feat: adding frame4 in GUI

---
+++ GUI/Implementation/build/tmp_data.py
@@ -0,0 +1,23 @@
+table_columns = ["ID", "Customer", "Product"]
+table_data = [    
+    ["1", "Jack", "Product 1"],
+    ["2", "Abby", "Product 1"],
+    ["3", "Abby", "Product 1"],
+    ["4", "Max", "Product 2"],
+    ["5", "Jack", "Product 2"],
+    ["6", "Abby", "Product 2"],
+    ["7", "Max", "Product 3"],
+    ["8", "Jack", "Product 3"],
+    ["9", "Abby", "Product 3"],
+    ["10", "Abby", "Product 3"],
+    ["11", "Jack", "Product 4"],
+    ["12", "Abby", "Product 4"],
+    ["13", "Abby", "Product 4"],
+    ["14", "Max", "Product 5"],
+    ["15", "Jack", "Product 5"],
+    ["16", "Abby", "Product 5"],
+    ["17", "Max", "Product 6"],
+    ["18", "Jack", "Product 6"],
+    ["19", "Abby", "Product 6"],
+    ["20", "Abby", "Product 6"],
+]
